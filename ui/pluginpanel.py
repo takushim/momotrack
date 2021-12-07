5f0c1524b0769eef52caa4aa80c37f7250f0dd6e
Takushi Miyoshi <tmiyoshi@nih.gov>
2021-12-07 18:16:48 -0500

    Refined reloading of plugin widgets

--- ui/pluginpanel.py
+++ ui/pluginpanel.py
@@ -12,26 +12,21 @@ class PluginPanel:
         self.ui.label_plugin.setText("Plugin: {0}".format(name))
 
     def update_widgets (self, plugin_class):
-        self.delete_plugin_widgets(self.ui.vlayout_plugin)
+        self.recurse_plugin_widgets(self.ui.vlayout_plugin, lambda x: x.deleteLater(), lambda x: x.deleteLater())
 
         self.plugin_class = plugin_class
         self.plugin_class.init_widgets(self.ui.vlayout_plugin)
         self.plugin_class.connect_signals()
 
-        self.set_policy_of_plugin_widgets(self.ui.vlayout_plugin)
+        self.recurse_plugin_widgets(self.ui.vlayout_plugin, \
+                                    lambda x: x.setSizeConstraint(QLayout.SetMinimumSize), \
+                                    lambda x: x.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum))
 
-    def delete_plugin_widgets (self, layout):
-        for index in reversed(range(layout.count())):
-            if layout.itemAt(index).widget() is None:
-                self.delete_plugin_widgets(layout.itemAt(index).layout())
+    def recurse_plugin_widgets (self, object, func_layout, func_widget):
+        for index in reversed(range(object.count())):
+            if object.itemAt(index).widget() is None:
+                self.recurse_plugin_widgets(object.itemAt(index).layout(), func_layout, func_widget)
+                func_layout(object.itemAt(index).layout())
             else:
-                layout.itemAt(index).widget().deleteLater()
-
-    def set_policy_of_plugin_widgets (self, layout):
-        for index in reversed(range(layout.count())):
-            if layout.itemAt(index).widget() is None:
-                layout.itemAt(index).layout().setSizeConstraint(QLayout.SetMinimumSize)
-                self.set_policy_of_plugin_widgets(layout.itemAt(index).layout())
-            else:
-                layout.itemAt(index).widget().setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+                func_widget(object.itemAt(index).widget())
 
