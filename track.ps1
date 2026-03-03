80848808051bfc2a7db8c575f93b67c44e6e5314
Takushi Miyoshi <tmiyoshi26@siumed.edu>
2026-03-02 20:38:26 -0600

    Update file names

--- track.ps1
+++ track.ps1
@@ -10,7 +10,7 @@ if (($help -eq $true) -or ($file.length -eq 0)) {
 
 # default values
 $ErrorActionPreference = 'Stop'
-$script = (Get-Command "mmtrack.py").Path
+$script = (Get-Command "momotrack.py").Path
 $stem = "{0}" -f (get-item $file).basename
 $record = "{0}_track.json" -f $stem
 
